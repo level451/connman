90f31bdfe358db3db9eaaca367fd0e6a4310382c
Todd <twitzel@gmail.com>
2018-12-11 12:41:39 -0700

    pp

--- app.js
+++ app.js
@@ -79,14 +79,14 @@ function connectssid(ssidWanted,passphrase) {
             console.log(`stdout: ${stdout}`);
             console.log(`stderr: ${stderr}`);
             fs.mkdirSync('/var/lib/connman/'+p)
-            var settingsFile = '['+p+']\nName='+s+'\nPassphrase='+passphrase+'\n'
+            var settingsFile = '['+p+']\nType=wifi\nName='+s+'\nPassphrase='+passphrase+'\n'
 
             fs.writeFileSync('/var/lib/connman/'+p+'/settings',settingsFile)
             exec('connmanctl connect '+p, (error, stdout, stderr) => {
             console.log('connection results')
                 console.log(`stdout: ${stdout}`);
                 console.log(`stderr: ${stderr}`);
-                cb()
+
             })
         })
     }
