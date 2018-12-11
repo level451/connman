cfdaf6ca9e45635ed844c97face959eb60dc34a4
Todd <twitzel@gmail.com>
2018-12-11 09:45:37 -0700

    init

---
+++ app.js
@@ -0,0 +1,20 @@
+
+var exec = require('child_process').exec;
+console.log('scanning wifi');
+exec('connmanctl scan wifi', (error, stdout, stderr) => {
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    var child = exec('connmanctl services');
+    child.stdout.on('data', function(data) {
+        console.log(data+'%');
+        res = data.split('\n');
+        console.log(res[0].split('      '));
+
+    });
+    child.stderr.on('data', function(data) {
+        console.log('stdoerr: ' + data)
+    });
+    child.on('close', function(code) {
+        console.log('closing code: ' + code);
+    });
+
