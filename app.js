70d87a4b5b23e51905cd4c73537b83dc43c5d955
Todd <twitzel@gmail.com>
2018-12-11 09:51:53 -0700

    init

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ exec('connmanctl scan wifi', (error, stdout, stderr) => {
     console.log(`stdout: ${stdout}`);
     console.log(`stderr: ${stderr}`);
     var child = exec('connmanctl services');
-    child.stdout.on('data', function(data) {
-        console.log(data+'%');
+    child.stdout.on('data', function (data) {
+        console.log(data + '%');
         res = data.split('\n');
         console.log(res[0].split('      '));
 
     });
-    child.stderr.on('data', function(data) {
+    child.stderr.on('data', function (data) {
         console.log('stdoerr: ' + data)
     });
-    child.on('close', function(code) {
+    child.on('close', function (code) {
         console.log('closing code: ' + code);
     });
 
+}
